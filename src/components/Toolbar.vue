128c47b8cfc8981856f23279a0cfcbff851a0e95
Iván Juárez <ivan@blindgazer.com>
2019-05-16 22:51:58 -0500

    refact: renaming toobar components

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Toolbar">
+    <toolbar-button icon="play-triangle" hint="Run Benchmark" />
+  </div>
+</template>
+
+<script>
+import ToolbarButton from '@/components/ToolbarButton.vue'
+
+export default {
+  components: {
+    ToolbarButton,
+  },
+};
+</script>
+
+<style lang="stylus" src="styles/components/Toolbar.styl"></style>
---
+++ src/components/ToolbarButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :title="hint" class="ToolbarButton">
+      <svg class="ToolbarButton-Icon">
+          <use :xlink:href="`#icon-${icon}`"/>
+      </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToolbarButton',
+  components: {},
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    hint: {
+      type: String,
+      default: '',
+    },
+  }
+};
+</script>
+
+<style lang="stylus" src="styles/components/ToolbarButton.styl"></style>
