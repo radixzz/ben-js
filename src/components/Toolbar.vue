4fe94ef9a17a0dda35d3e1e2a94e18b04ed68c99
Iván Juárez <ivan@blindgazer.com>
2019-05-19 21:22:48 -0500

    feat: label edit component to handle bench title edits

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="Toolbar">
-    <toolbar-button icon="play-triangle" hint="Run Benchmark" />
+    <label-edit :text="title" @change="updateTitle"></label-edit>
+    <div class="Toolbar-BenchActions">
+      <toolbar-button icon="play-triangle" hint="Run Benchmark" />
+    </div>
   </div>
 </template>
 
 <script>
 import ToolbarButton from '@/components/ToolbarButton.vue'
+import LabelEdit from '@/components/LabelEdit.vue'
 
 export default {
   components: {
     ToolbarButton,
+    LabelEdit,
   },
+  data() {
+    return {
+      title: 'Untitled Benchmark'
+    }
+  },
+  methods: {
+    updateTitle(title) {
+      this.title = title;
+    }
+  }
 };
 </script>
 
