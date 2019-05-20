60722a172708fef9f8cec66ac84ec1bcc2cebf01
Iván Juárez <ivan@blindgazer.com>
2019-05-19 21:23:36 -0500

    feat: handling model changes and layout resize

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Editor">
+  <div class="Editor" v-viewport-size:debounce.100="onResize">
     <div ref="editor" class="Editor-Container"></div>
   </div>
 </template>
@@ -9,20 +9,64 @@ import * as monaco from 'monaco-editor';
 import '@/scripts/editor-env';
 
 export default {
-  components: {
+  data() {
+    return {
+      initialized: false,
+      editor: null,
+    }
+  },
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
   },
   mounted() {
-    monaco.editor.create(this.$refs.editor, {
-      value: [
-        'console.log("Editor!!");',
-      ].join('\n'),
-      language: 'javascript',
-      theme: 'vs-dark',
-      minimap: {
-        enabled: false,
-      }
-    });
+    this.createEditor();
+    this.initialized = true;
   },
+  methods: {
+    createEditor() {
+      this.editor = monaco.editor.create(this.$refs.editor, {
+        value: this.value,
+        language: 'javascript',
+        theme: 'vs-dark',
+        minimap: {
+          enabled: false,
+        },
+      });
+      this.editor.onDidChangeModelContent(this.onModelChange)
+    },
+    onModelChange() {
+      this.$emit('change', this.editor.getValue());
+    },
+    onResize() {
+      if (this.initialized) {
+        const { $el, editor } = this;
+        const { width, height } = $el.getBoundingClientRect();
+        editor.layout({ width, height });
+      }
+    }
+  },
+  watch: {
+    active(value) {
+      if (value) {
+        this.$nextTick(() => {
+          this.onResize();
+        })
+      }
+    },
+    value(model) {
+      const { editor } = this;
+      if (this.value !== model) {
+        editor.setValue(model);
+      }
+    }
+  }
 };
 </script>
 
