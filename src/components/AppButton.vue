acfffdb2e5ae5fc8a1f4b00e109e0fcea1ebab01
Iván Juárez <ivan@blindgazer.com>
2019-06-02 14:57:43 -0500

    feat(front): generic button to be used inside toolbar

---
+++ src/components/AppButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    :title="hint"
+    class="AppButton"
+    @click="onClick"
+  >
+      <svg class="AppButton-Icon">
+          <use :xlink:href="`#icon-${icon}`"/>
+      </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  components: {},
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    hint: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    }
+  }
+};
+</script>
+
+<style lang="stylus" src="styles/components/AppButton.styl"></style>
