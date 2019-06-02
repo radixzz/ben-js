8dab96a131a71d5d6e3e16000b35065790ba2fc3
Iván Juárez <ivan@blindgazer.com>
2019-06-02 15:09:39 -0500

    feat: generic layout component

---
+++ src/components/layout/LayoutModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="LayoutModal">
+    <div class="LayoutModal-Background">
+    </div>
+    <section class="LayoutModal-Wrapper">
+      <div class="LayoutModal-TitleBar">
+        <h1>{{ title }}</h1>
+        <app-button
+          @click="closeModal"
+          class="LayoutModal-CloseButton"
+          title="Close"
+          icon="cross"
+        />
+      </div>
+      <div class="LayoutModal-Content">
+        <slot></slot>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import AppButton from '@/components/AppButton.vue'
+
+export default {
+  name: 'LayoutModal',
+  components: {
+    AppButton,
+  },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    }
+  }
+};
+</script>
+
+<style lang="stylus" src="styles/components/layout/LayoutModal.styl"></style>
