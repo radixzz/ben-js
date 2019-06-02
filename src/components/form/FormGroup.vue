312db82630eecf50185324111793b55d8e6e87bf
Iván Juárez <ivan@blindgazer.com>
2019-06-02 15:01:03 -0500

    feat: form group as component

---
+++ src/components/form/FormGroup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="FormGroup">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormGroup',
+  components: {},
+};
+</script>
+
+<style lang="stylus" src="styles/components/form/FormGroup.styl"></style>
