65f2578984ad2419808e479c92c59e1e00185453
Iván Juárez <ivan@blindgazer.com>
2019-06-18 21:45:51 -0500

    feat: login view

---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="Login">
+    <div
+      class="Login-Content"
+    >
+      <h1>Save your work with GitHub</h1>
+      <p>
+        BenJS is fully dependant on GitHub for saving and sharing your tests online.
+        When you create a test you are actually creating a <strong>public gist</strong> in your account.
+      </p>
+      <div
+        class="Login-Button"
+        @click="onGitLoginClick"
+      >
+        <svg><use xlink:href="#icon-github"/></svg>
+        Login with GitHub
+      </div>
+      <div
+        class="Login-LocalLink"
+      >Continue using local storage</div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import { AUTH_SIGN_IN } from '@/store/modules/auth'
+
+export default {
+  components: {
+  },
+  methods: {
+    onGitLoginClick() {
+      this.signIn();
+    },
+    ...mapActions({
+      signIn: AUTH_SIGN_IN
+    }),
+  }
+};
+</script>
+
+<style lang="stylus" src="styles/views/Login.styl"></style>
