78f6f67dec75225668bc122e26a16c802db7131c
Iván Juárez <ivan@blindgazer.com>
2019-06-26 19:07:00 -0500

    feat: worskpace console and worskpace sidebar components

---
+++ src/components/workspace/WorkspaceConsole.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="WorkspaceConsole">
+
+  </nav>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="stylus" src="styles/components/workspace/WorkspaceConsole.styl"></style>
---
+++ src/components/workspace/WorkspaceSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="WorkspaceSidebar">
+
+  </nav>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="stylus" src="styles/components/workspace/WorkspaceSidebar.styl"></style>
