0043f25ff974e93123e27f53516a682032368a26
Iván Juárez <ivan@blindgazer.com>
2019-06-27 18:30:53 -0500

    feat: sketching database module

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from './modules/auth';
+import database from './modules/database';
 import editors from './modules/editors';
 
 Vue.use(Vuex);
@@ -9,6 +10,7 @@ export default new Vuex.Store({
   state: {},
   modules: {
     auth,
+    database,
     editors,
   },
 });
