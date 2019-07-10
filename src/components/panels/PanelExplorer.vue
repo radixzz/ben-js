9aabe1e7d402daebb05030988741c5d0b6d6b07c
Iván Juárez <ivan@blindgazer.com>
2019-07-10 13:57:16 -0500

    feat: setting up editor panels

---
+++ src/components/panels/PanelExplorer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="PanelExplorer">
+    <panel title="Your Projects">
+    </panel>
+  </div>
+</template>
+
+<script>
+import Panel from '@/components/panels/Panel.vue';
+
+export default {
+  components: {
+    Panel,
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="stylus" src="styles/components/panels/PanelExplorer.styl"></style>
---
+++ src/components/panels/PanelProject.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="PanelProject">
+    <panel
+      title="Project Settings"
+    >
+      <form-text-area
+        id="project-description"
+        class="PanelProject-TextArea"
+        placeholder="What are you testing?"
+        :maxLength='250'
+        :rows='5'
+        label="Description:"
+      />
+      <widget-libraries
+        :items='libraries'
+      />
+    </panel>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Panel from '@/components/panels/Panel.vue';
+import WidgetLibraries from '@/components/widgets/WidgetLibraries.vue';
+import FormTextArea from '@/components/form/FormTextArea.vue';
+
+export default {
+  components: {
+    Panel,
+    WidgetLibraries,
+    FormTextArea,
+  },
+  methods: {
+
+  },
+  computed: {
+    ...mapState({
+      libraries: state => state.workspace.libraries
+    })
+  }
+};
+</script>
+
+<style lang="stylus" src="styles/components/panels/PanelProject.styl"></style>
