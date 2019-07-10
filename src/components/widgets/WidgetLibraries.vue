5aeee2fa39644d08955bc6af07c583c860ab88a0
Iván Juárez <ivan@blindgazer.com>
2019-07-10 15:38:43 -0500

    feat: widget component basic listing

---
+++ src/components/widgets/WidgetLibraries.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="WidgetLibraries">
+    <h3 class="WidgetLibraries-Title">External Libraries</h3>
+    <ul>
+      <li
+        class="WidgetLibraries-Item"
+        v-for="(item, idx) in items"
+        :key="idx"
+        :title="item.name"
+      >
+        <svg class="WidgetLibraries-ItemIcon">
+          <use xlink:href='#icon-arrow-right'/>
+        </svg>
+        <div class="WidgetLibraries-ItemInfo">
+          <div>{{ item.name }}</div>
+          <div>{{ item.version }}</div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  props: {
+    items: {
+      type: Object,
+      default: () => [
+        { name: 'three', version: '0.102.5' },
+        { name: 'lodash', version: '1.23' }
+      ],
+    }
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="stylus" src="styles/components/widgets/WidgetLibraries.styl"></style>
