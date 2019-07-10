06cebbe19ce11f3d11b35541124a1da77dcb3d7b
Iván Juárez <ivan@blindgazer.com>
2019-07-10 15:39:15 -0500

    feat: sidebar components

--- src/components/workspace/WorkspaceSidebar.vue
+++ src/components/workspace/WorkspaceSidebar.vue
@@ -1,13 +1,28 @@
 <template>
   <nav class="WorkspaceSidebar">
-
+    <panes-container
+      :sizes='[70, 30]'
+      direction="vertical"
+    >
+      <panel-project />
+      <panes-split/>
+      <panel-explorer />
+    </panes-container>
   </nav>
 </template>
 
 <script>
+import PanesContainer from '@/components/panes/PanesContainer.vue';
+import PanesSplit from '@/components/panes/PanesSplit.vue';
+import PanelProject from '@/components/panels/PanelProject.vue';
+import PanelExplorer from '@/components/panels/PanelExplorer.vue';
 
 export default {
   components: {
+    PanesContainer,
+    PanesSplit,
+    PanelProject,
+    PanelExplorer,
   },
   methods: {
   },
