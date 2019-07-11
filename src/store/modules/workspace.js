d1142ef1bb7ba0a5657cde54ae11510aecddeba3
Iván Juárez <ivan@blindgazer.com>
2019-07-11 14:03:43 -0500

    feat: title and description actions

--- src/store/modules/workspace.js
+++ src/store/modules/workspace.js
@@ -1,7 +1,9 @@
-export const LOAD_EDITOR_CONTENTS = 'editors/load-editor-content';
-export const LOAD_EDITOR_SETTINGS = 'editors/load-editor-settings';
-export const SET_EDITOR_CONTENT = 'editors/set-editor-content';
-export const SET_EDITOR_SETTING = 'editors/set-editor-settings';
+export const LOAD_CONTENTS = 'workspace/load-contents';
+export const LOAD_SETTINGS = 'workspace/load-settings';
+export const SET_CONTENT = 'workspace/set-content';
+export const SET_DESCRIPTION = 'workspace/set-description';
+export const SET_TITLE = 'workspace/set-title';
+export const SET_SETTING = 'workspace/set-settings';
 
 const state = {
   editors: [],
@@ -9,7 +11,7 @@ const state = {
     { name: "three", version: "0.106.2" },
     { name: "lodash-minimal-development-version", version: "4.17.11" },
   ],
-  title: "",
+  title: "Untitled",
   description: "",
 };
 
@@ -21,37 +23,48 @@ function findEditorById({ editors }, id) {
 
 const actions = {
   // [LOAD_EDITORS]({ commit }) {},
-  [LOAD_EDITOR_CONTENTS]({ commit }, { id }) {
+  [LOAD_CONTENTS]({ commit }, { id }) {
     // Retrieve from Local Storage or API
     const content = '';
     const editor = findEditorById(state, id);
-    commit(SET_EDITOR_CONTENT, { editor, content });
+    commit(SET_CONTENT, { editor, content });
   },
-  [LOAD_EDITOR_SETTINGS]({ state, commit }, { id }) {
+  [LOAD_SETTINGS]({ state, commit }, { id }) {
     // Retrieve from Local Storage or API
     const settings = {};
     const editor = findEditorById(state, id);
     Object.keys(settings).forEach((key) => {
-      commit(SET_EDITOR_SETTING, {
+      commit(SET_SETTING, {
         editor,
         name: key,
         value: settings[key]
       });
     });
+  },
+  [SET_TITLE]({ commit }, title) {
+    commit(SET_TITLE, title)
+  },
+  [SET_DESCRIPTION]({ commit }, title) {
+    commit(SET_DESCRIPTION, title)
   }
 };
 
 const mutations = {
-  [SET_EDITOR_CONTENT](_, { editor, content }) {
+  [SET_CONTENT](_, { editor, content }) {
     editor.content = content;
   },
-  [SET_EDITOR_SETTING](_, { editor, name, value }) {
+  [SET_SETTING](_, { editor, name, value }) {
     editor.settings[name] = value;
+  },
+  [SET_TITLE](state, title) {
+    state.title = title;
+  },
+  [SET_DESCRIPTION](state, description) {
+    state.description = description;
   }
 };
 
 export default {
-  namespaced: true,
   state,
   getters,
   actions,
