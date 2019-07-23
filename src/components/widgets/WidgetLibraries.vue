e8ad1a0745228998e152fe5127dbda0aef48bd46
Iván Juárez <ivan@blindgazer.com>
2019-07-23 10:57:53 -0500

    feat: drag libraries and cdnjs searchbox as component

--- src/components/widgets/WidgetLibraries.vue
+++ src/components/widgets/WidgetLibraries.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="WidgetLibraries">
-    <form-edit
-      class="WidgetLibraries-Searchbox"
-      label="External Scripts"
-      placeholder="Type to search from CDNjs (lodash, rxjs, ramda, etc)"
-    >
-      <svg><use xlink:href="#icon-magnifier"/></svg>
-      <ul class="WidgetLibraries-Results" v-show="showResults">
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </form-edit>
+    <widget-libraries-searchbox
+      @add="onLibraryAdd"
+    />
     <div class="WidgetLibraries-Info">
       <svg><use xlink:href="#icon-information"/></svg>
       <p>
@@ -19,21 +10,28 @@
         The scope of this libraries will be limited to current test case.
       </p>
     </div>
-    <ul class="WidgetLibraries-List">
-      <li
-        class="WidgetLibraries-Item"
-        v-for="(item, idx) in items"
-        :key="idx"
-        :title="item.name"
-      >
-        <div class="WidgetLibraries-DraggableInput">
-          <div class="WidgetLibraries-DragHandle" title="Drag to reorder">
-            <svg><use xlink:href="#icon-drag-dots"/></svg>
+    <draggable
+      handle=".WidgetLibraries-DragHandle"
+      class="WidgetLibraries-List"
+      v-bind="dragOptions"
+      tag="ul"
+    >
+      <transition-group type="transition" name="WidgetLibraries--flip">
+        <li
+          class="WidgetLibraries-Item"
+          v-for="item in items"
+          :key="item.order"
+          :title="item.name"
+        >
+          <div class="WidgetLibraries-DraggableInput">
+            <div class="WidgetLibraries-DragHandle" title="Drag to reorder">
+              <svg><use xlink:href="#icon-drag-dots"/></svg>
+            </div>
+            <input type="text" v-model="item.url" placeholder="https://domain.com/your-script.js">
           </div>
-          <input type="text" v-model="item.url" placeholder="https://domain.com/your-script.js">
-        </div>
-      </li>
-    </ul>
+        </li>
+      </transition-group>
+    </draggable>
     <app-button
       class="WidgetLibraries-AddScript"
       icon="icon-plus"
@@ -42,28 +40,40 @@
 </template>
 
 <script>
-import FormEdit from '@/components/form/FormEdit.vue';
-import AppButton from '@/components/AppButton.vue'
+
+import draggable from 'vuedraggable';
+import WidgetLibrariesSearchbox from '@/components/widgets/WidgetLibrariesSearchbox.vue';
+import AppButton from '@/components/AppButton.vue';
 
 export default {
   components: {
-    FormEdit,
-    AppButton
-  },
-  data() {
-    return {
-      showResults: false,
-    }
-  },
-  props: {
-    items: {
-      type: Array,
-      default: () => [],
-    }
+    WidgetLibrariesSearchbox,
+    AppButton,
+    draggable
   },
   methods: {
+    onLibraryAdd(library) {
+      console.log('onLibraryAdd', library)
+    },
+    clearEmptyFields() {
 
+    }
   },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        disabled: false,
+        ghostClass: "WidgetLibraries-Item--ghost"
+      };
+    },
+    items() {
+      return [
+        { order: 0, name: '', url: '' },
+        { order: 1, name: '', url: '' },
+      ];
+    }
+  }
 };
 </script>
 
