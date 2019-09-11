129f5a8b7c299ac77724bb24d2b5e018ef1686a0
Iván Juárez Núñez <ivan@blindgazer.com>
2019-09-11 00:21:50 -0500

    fix: optimizing loaded modules of monaco-editor

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,7 @@ module.exports = {
     plugins: [
       new MonacoWebpackPlugin({
         languages: ['typescript', 'javascript', 'html'],
+        output: 'monaco',
       }),
       new CopyPlugin([
         { from: 'static', to: './' },
