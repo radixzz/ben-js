41eaf973000f7be1048725fee1a51c1116579ab7
Iván Juárez Núñez <ivan@blindgazer.com>
2019-09-12 16:19:05 -0500

    feat: splitting workspace actions

--- src/store/modules/workspace.js
+++ src/store/modules/workspace.js
@@ -1,5 +1,6 @@
 import {
-  WORKSPACE_UPDATE,
+  WORKSPACE_UPDATE_TITLE,
+  WORKSPACE_UPDATE_DESCRIPTION,
   WORKSPACE_RESET,
   WORKSPACE_SIDEBAR,
 } from './types/action-types';
@@ -27,13 +28,18 @@ const state = {
 const getters = {};
 
 const actions = {
-  [WORKSPACE_UPDATE]({ commit }, { slug, title, description }) {
-    if (slug !== state.slug) commit(WORKSPACE_SET_SLUG, slug)
-    if (title !== state.title) commit(WORKSPACE_SET_TITLE, title)
+  [WORKSPACE_UPDATE_TITLE]({ commit }, title) {
+    if (title !== state.title) {
+      commit(WORKSPACE_SET_TITLE, title)
+      commit(WORKSPACE_SET_SLUG, 'slug')
+    }
+  },
+  [WORKSPACE_UPDATE_DESCRIPTION]({ commit }, description) {
     if (description !== state.description) commit(WORKSPACE_SET_DESCRIPTION, description);
   },
   [WORKSPACE_RESET]({ dispatch }) {
-    dispatch(WORKSPACE_UPDATE, { ...DefaultProps });
+    dispatch(WORKSPACE_UPDATE_TITLE, DefaultProps.title);
+    dispatch(WORKSPACE_UPDATE_DESCRIPTION, DefaultProps.description);
   },
   [WORKSPACE_SIDEBAR]({ commit }, payload) {
     commit(WORKSPACE_SET_SIDEBAR, payload)
