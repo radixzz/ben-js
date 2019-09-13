12d282d6aa07df388c6db47cd74d98703207c680
Iván Juárez Núñez <ivan@blindgazer.com>
2019-09-13 02:35:49 -0500

    feat: more flexible helper to map store fields

--- src/mixins/MixinWorkspace.js
+++ src/mixins/MixinWorkspace.js
@@ -1,3 +1,4 @@
+
 import {
   WORKSPACE_UPDATE_TITLE,
   WORKSPACE_UPDATE_DESCRIPTION,
@@ -5,30 +6,71 @@ import {
   WORKSPACE_RESET,
 } from '@/store/modules/types/action-types';
 
-function mapProperties(self, moduleName, items) {
-  return items.reduce((dict, item) => {
-    const { action, prop } = item;
-    Object.defineProperty(dict, prop, {
+function mockPropsPath(obj, path, value) {
+  const props = path.split('.');
+  let lastKey = null;
+  if (arguments.length === 3) {
+    lastKey = props.pop();
+  }
+  let headProp = obj;
+  for (let i = 0; i < props.length; i++) {
+    const name = props[i];
+    headProp[name] = headProp[name] || {};
+    headProp = headProp[name];
+  }
+  if (lastKey) {
+    /* eslint-disable-next-line */
+    headProp[lastKey] = value;
+  }
+  return obj;
+}
+
+function getNestedProp(obj, path) {
+  const keys = path.split('.');
+  return keys.reduce((prev, k) => prev && prev[k], obj);
+}
+
+function bindFields(store, storePath, fields) {
+  const obj = {};
+  const storeModule = getNestedProp(store.state, storePath);
+  fields.forEach((field) => {
+    mockPropsPath(obj, field.prop, null);
+    const parentPath = field.prop.split('.').slice(0, -1).join('.');
+    const parentProp = getNestedProp(obj, parentPath) || obj;
+    const targetKey = field.prop.split('.').pop();
+    Object.defineProperty(parentProp, targetKey, {
       configurable: true,
-      set(val) {
-        self.$store.dispatch(action, val);
-      },
       get() {
-        return self.$store.state[moduleName][prop];
+        return getNestedProp(storeModule, field.prop);
       },
-    });
-    return dict;
-  }, {});
+      set(val) {
+        store.dispatch(field.action, val);
+      }
+    })
+  })
+  return obj;
+}
+
+function mapStoreModule(storePath, fields) {
+  let cacheEntry = null;
+  const props = {
+    get() {
+      if (!cacheEntry) {
+        cacheEntry = bindFields(this.$store, storePath, fields)
+      }
+      return cacheEntry;
+    }
+  };
+  const root = mockPropsPath({}, storePath, props);
+  return root;
 }
 
 export default {
-  data() {
-    return {
-      workspace: mapProperties(this, 'workspace', [
-        { action: WORKSPACE_UPDATE_TITLE, prop: 'title' },
-        { action: WORKSPACE_UPDATE_DESCRIPTION, prop: 'description' },
-        { action: WORKSPACE_SIDEBAR, prop: 'sidebar.visible' },
-      ])
-    }
-  },
+  computed: {
+    ...mapStoreModule('workspace', [
+      { action: WORKSPACE_UPDATE_TITLE, prop: 'title' },
+      { action: WORKSPACE_UPDATE_DESCRIPTION, prop: 'description' },
+      { action: WORKSPACE_SIDEBAR, prop: 'sidebar.visible' },
+    ])
+  }
 }
