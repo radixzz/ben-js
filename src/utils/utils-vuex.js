7fe509e9ca514cabe5dce8d93b3d2c7a2f5c3f6d
Iván Juárez Núñez <ivan@blindgazer.com>
2019-09-19 17:47:15 -0500

    refact: replacing hand-mande with lodash functions

--- src/utils/utils-vuex.js
+++ src/utils/utils-vuex.js
@@ -1,45 +1,22 @@
-
-function mockPropsPath(obj, path, value) {
-  const props = path.split('.');
-  let lastKey = null;
-  if (arguments.length === 3) {
-    lastKey = props.pop();
-  }
-  let headProp = obj;
-  for (let i = 0; i < props.length; i++) {
-    const name = props[i];
-    headProp[name] = headProp[name] || {};
-    headProp = headProp[name];
-  }
-  if (lastKey) {
-    /* eslint-disable-next-line */
-    headProp[lastKey] = value;
-  }
-  return obj;
-}
-
-function getNestedProp(obj, path) {
-  const keys = path.split('.');
-  return keys.reduce((prev, k) => prev && prev[k], obj);
-}
+import { get, set } from 'lodash';
 
 export function bindFields(store, storePath, fields) {
   const obj = {};
-  const storeModule = getNestedProp(store.state, storePath);
+  const storeModule = get(store.state, storePath);
   fields.forEach((field) => {
     const prop = field.prop || field.getter;
-    mockPropsPath(obj, prop, null);
+    set(obj, prop, null);
     const parentPath = prop.split('.').slice(0, -1).join('.');
-    const parentProp = getNestedProp(obj, parentPath) || obj;
+    const parentProp = get(obj, parentPath) || obj;
     const targetKey = prop.split('.').pop();
     Object.defineProperty(parentProp, targetKey, {
       configurable: true,
       get() {
         if (field.getter) {
-          return getNestedProp(store.getters, field.getter);
+          return get(store.getters, field.getter);
         }
         // read directly from store state object
-        return getNestedProp(storeModule, field.prop);
+        return get(storeModule, field.prop);
       },
       set(val) {
         if (field.setter) {
@@ -64,6 +41,6 @@ export function mapStateFields(storePath, fields) {
       return cacheEntry;
     }
   };
-  const root = mockPropsPath({}, storePath, props);
+  const root = set({}, storePath, props);
   return root;
 }
